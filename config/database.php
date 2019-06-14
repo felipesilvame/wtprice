28764b4faa5384bf510daf4a79340ada0e7ffff7
Felipe Silva <felipe.silvame@alumnos.usm.cl>
2019-06-13 21:16:56 -0400

    base code

--- config/database.php
+++ config/database.php
@@ -35,7 +35,7 @@ return [
     'connections' => [
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => env('DB_DATABASE', database_path('dev.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
