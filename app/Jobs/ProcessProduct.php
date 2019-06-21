58c7b0f6038d25823fb953b577afbcbc4b4a2f82
Felipe Silva <felipe.silvame@alumnos.usm.cl>
2019-06-21 18:06:06 -0400

    fixed umbral rata

--- app/Jobs/ProcessProduct.php
+++ app/Jobs/ProcessProduct.php
@@ -47,6 +47,8 @@ class ProcessProduct implements ShouldQueue
       }else if ((!$this->product->ultima_actualizacion) || $this->product->ultima_actualizacion->diffInMinutes() >= $this->product->intervalo_actualizacion) {
         $client = new \GuzzleHttp\Client();
         $tienda = Tienda::findOrFail($this->product->id_tienda);
+        $request = null;
+        $response = null;
         $url = "";
         if ($tienda->protocolo) {
           $url .= $tienda->protocolo."://";
@@ -69,163 +71,177 @@ class ProcessProduct implements ShouldQueue
         if ($tienda->request_body_sku && $tienda->method == "POST") {
           $options['body'] = json_encode([$tienda->request_body_sku => $this->product->sku]);
         }
-        if ($tienda->method == "POST") {
-          $response = $client->post($url, $options)->getBody()->getContents();
-        }
-        else {
-          if ($tienda->headers) {
-            $request = new \GuzzleHttp\Psr7\Request($tienda->method, $url, $tienda->headers);
-          }
-          else{
-            $request = new \GuzzleHttp\Psr7\Request($tienda->method, $url);
-          }
-
-          $response = (string) $client->send($request)->getBody();
-        }
-        // if ($tienda->headers) {
-        // }else {
-        //   $request = new \GuzzleHttp\Psr7\Request($tienda->method, $url);
-        // }
-        $data = null;
         try {
-          $data = json_decode($response, true);
+          if ($tienda->method == "POST") {
+            $response = $client->post($url, $options)->getBody()->getContents();
+          }
+          else {
+            if ($tienda->headers) {
+              $request = new \GuzzleHttp\Psr7\Request($tienda->method, $url, $tienda->headers);
+            }
+            else{
+              $request = new \GuzzleHttp\Psr7\Request($tienda->method, $url);
+            }
+
+            $response = (string) $client->send($request)->getBody();
+          }
         } catch (\Exception $e) {
-          Log::warning("Producto id ".$this->product->id.": No se ha podido convertir la respuesta a JSON");
-          $this->product->intentos_fallidos +=1 ;
+          Log::error("No se ha podido obtener respuesta del servidor para el producto ".$this->product->id." Tienda ".$tienda->nombre);
+          $this->product->intentos_fallidos += 1;
         }
-        $precio_referencia = null;
-        $precio_oferta = null;
-        $precio_tarjeta = null;
-        $nombre_producto = null;
-        if ($data) {
-          if (!$this->product->url_compra) {
-            try {
-              if ($tienda->campo_slug_compra) {
-                $url_compra = ArrHelper::get_pipo($data, $tienda->campo_slug_compra);
-                if ($url_compra) {
-                  $this->product->url_compra = $tienda->url_prefix_compra.$url_compra.$tienda->url_suffix_compra;
+        if ((boolean) $response){
+          $data = null;
+          try {
+            $data = json_decode($response, true);
+          } catch (\Exception $e) {
+            Log::warning("Producto id ".$this->product->id.": No se ha podido convertir la respuesta a JSON");
+            $this->product->intentos_fallidos +=1 ;
+          }
+          $precio_referencia = null;
+          $precio_oferta = null;
+          $precio_tarjeta = null;
+          $nombre_producto = null;
+          if ($data) {
+            if (!$this->product->url_compra) {
+              try {
+                if ($tienda->campo_slug_compra) {
+                  $url_compra = ArrHelper::get_pipo($data, $tienda->campo_slug_compra);
+                  if ($url_compra) {
+                    $this->product->url_compra = $tienda->url_prefix_compra.$url_compra.$tienda->url_suffix_compra;
+                    $this->product->save();
+                  }
+                } else if ((boolean)$tienda->url_prefix_compra) {
+                  //try to guess url compra with the sku
+                  $this->product->url_compra = $tienda->url_prefix_compra.(string)$this->product->sku.$tienda->url_suffix_compra;
                   $this->product->save();
                 }
-              } else if ((boolean)$tienda->url_prefix_compra) {
-                //try to guess url compra with the sku
-                $this->product->url_compra = $tienda->url_prefix_compra.(string)$this->product->sku.$tienda->url_suffix_compra;
-                $this->product->save();
+              } catch (\Exception $e) {
+                //does nothing... maybe log this?
+                Log::warning("Producto id".$this->product->id.": No se ha podido aplicar la url de compra");
+              }
+            }
+
+            try {
+              if ($tienda->campo_precio_tarjeta) {
+                $p_tarjeta = (integer)preg_replace('/[^0-9]/','',(string)ArrHelper::get_pipo($data, $tienda->campo_precio_tarjeta));
+                if ($p_tarjeta) {
+                  if ($p_tarjeta < 10000000) {
+                    $this->product->precio_tarjeta = $p_tarjeta;
+                  } else {
+                    $this->product->intentos_fallidos += 1;
+                  }
+                }
+
               }
             } catch (\Exception $e) {
-              //does nothing... maybe log this?
-              Log::warning("Producto id".$this->product->id.": No se ha podido aplicar la url de compra");
+              //Log::warning("Producto id ".$this->product->id.": No se ha podido obtener el precio de tarjeta");
+              //$this->product->intentos_fallidos +=1 ;
             }
-          }
-
-          try {
-            if ($tienda->campo_precio_tarjeta) {
-              $p_tarjeta = (integer)preg_replace('/[^0-9]/','',(string)ArrHelper::get_pipo($data, $tienda->campo_precio_tarjeta));
-              if ($p_tarjeta) {
-                $this->product->precio_tarjeta = $p_tarjeta;
+            try {
+              if ($tienda->campo_precio_referencia) {
+                $p_referencia = (integer)preg_replace('/[^0-9]/','',(string)ArrHelper::get_pipo($data, $tienda->campo_precio_referencia));
+                if ($p_referencia && $p_referencia < 10000000) {
+                  $this->product->precio_referencia = $p_referencia;
+                } else {
+                  $this->product->intentos_fallidos +=1;
+                }
               }
+            } catch (\Exception $e) {
+              Log::warning("Producto id ".$this->product->id.": No se ha podido obtener el precio de referencia");
+              $this->product->intentos_fallidos +=1 ;
+            }
+            try {
+              if ($tienda->campo_precio_oferta) {
+                $p_oferta = (integer)preg_replace('/[^0-9]/','',(string)ArrHelper::get_pipo($data, $tienda->campo_precio_oferta));
+                if ($p_oferta) {
+                  if ($p_oferta < 10000000) {
+                    $this->product->precio_oferta = $p_oferta;
+                    // code...
+                  } else {
+                    $this->product->intentos_fallidos +=1;
+                  }
+                }
 
-            }
-          } catch (\Exception $e) {
-            //Log::warning("Producto id ".$this->product->id.": No se ha podido obtener el precio de tarjeta");
-            //$this->product->intentos_fallidos +=1 ;
-          }
-          try {
-            if ($tienda->campo_precio_referencia) {
-              $p_referencia = (integer)preg_replace('/[^0-9]/','',(string)ArrHelper::get_pipo($data, $tienda->campo_precio_referencia));
-              if ($p_referencia) {
-                $this->product->precio_referencia = $p_referencia;
               }
+            } catch (\Exception $e) {
+              //Log::warning("Producto id ".$this->product->id.": No se ha podido obtener el precio oferta");
+              //$this->product->intentos_fallidos +=1 ;
             }
-          } catch (\Exception $e) {
-            Log::warning("Producto id ".$this->product->id.": No se ha podido obtener el precio de referencia");
-            $this->product->intentos_fallidos +=1 ;
-          }
-          try {
-            if ($tienda->campo_precio_oferta) {
-              $p_oferta = (integer)preg_replace('/[^0-9]/','',(string)ArrHelper::get_pipo($data, $tienda->campo_precio_oferta));
-              if ($p_oferta) {
-                $this->product->precio_oferta = $p_oferta;
+            try {
+              if ($tienda->campo_nombre_producto) {
+                $this->product->nombre = ArrHelper::get_pipo($data, $tienda->campo_nombre_producto);
               }
-
+            } catch (\Exception $e) {
+              Log::warning("Producto id ".$this->product->id.": No se ha podido obtener el nombre del producto");
+              $this->product->nombre = '-';
+              $this->product->intentos_fallidos +=1 ;
             }
-          } catch (\Exception $e) {
-            //Log::warning("Producto id ".$this->product->id.": No se ha podido obtener el precio oferta");
-            //$this->product->intentos_fallidos +=1 ;
-          }
-          try {
-            if ($tienda->campo_nombre_producto) {
-              $this->product->nombre = ArrHelper::get_pipo($data, $tienda->campo_nombre_producto);
-            }
-          } catch (\Exception $e) {
-            Log::warning("Producto id ".$this->product->id.": No se ha podido obtener el nombre del producto");
-            $this->product->nombre = '-';
-            $this->product->intentos_fallidos +=1 ;
-          }
-          // create historical data
-          $historical = HistorialPrecio::create([
+            // create historical data
+            $historical = HistorialPrecio::create([
             'id_producto' => $this->product->id,
             'precio_referencia' => $this->product->precio_referencia,
             'precio_oferta' => $this->product->precio_oferta,
             'precio_tarjeta' => $this->product->precio_tarjeta,
             'fecha' => Carbon::now(),
-          ]);
+            ]);
 
-          if (count($this->product->getDirty()) > 0) {
-            //save ultima actualizacion
-          }
-          $this->product->ultima_actualizacion = \Carbon\Carbon::now();
+            if (count($this->product->getDirty()) > 0) {
+              //save ultima actualizacion
+            }
+            $this->product->ultima_actualizacion = \Carbon\Carbon::now();
 
-          // check and create minimum
-          $minimo = $this->product->minimo;
-          if (!$minimo) {
-            $minimo = MinimoPrecio::create([
+            // check and create minimum
+            $minimo = $this->product->minimo;
+            if (!$minimo) {
+              $minimo = MinimoPrecio::create([
               'id_producto' => $this->product->id,
               'precio_referencia' => $this->product->precio_referencia,
               'precio_oferta' => $this->product->precio_oferta,
               'precio_tarjeta' => $this->product->precio_tarjeta,
               'fecha' => Carbon::now(),
-            ]);
-          } else {
-            if ((!$minimo->precio_referencia) || $minimo->precio_referencia > $this->product->precio_referencia) {
-              if ((boolean)$minimo->precio_referencia && $minimo->precio_referencia > $this->product->precio_referencia) {
-                //check how much it changes
-                $percentage_rata = (int)$minimo->precio_referencia-(int)$this->product->precio_referencia/(float)$minimo->precio_referencia;
-                if ($percentage_rata >= 0.5) {
-                  \Notification::route('slack', env('SLACK_WEBHOOK_URL'))
+              ]);
+            } else {
+              if ((!$minimo->precio_referencia) || $minimo->precio_referencia > $this->product->precio_referencia) {
+                if ((boolean)$minimo->precio_referencia && $minimo->precio_referencia > $this->product->precio_referencia) {
+                  //check how much it changes
+                  $percentage_rata = (int)$minimo->precio_referencia-(int)$this->product->precio_referencia/(float)$minimo->precio_referencia;
+                  if ($percentage_rata >= 0.5) {
+                    \Notification::route('slack', env('SLACK_WEBHOOK_URL'))
                     ->notify(new \App\Notifications\AlertaRata($this->product, $minimo->precio_referencia, $this->product->precio_referencia));
+                  }
                 }
+                $minimo->precio_referencia = $this->product->precio_referencia;
               }
-              $minimo->precio_referencia = $this->product->precio_referencia;
-            }
-            if ((!$minimo->precio_oferta) || $minimo->precio_oferta > $this->product->precio_oferta) {
-              if ((boolean)$minimo->precio_oferta && $minimo->precio_oferta > $this->product->precio_oferta) {
-                //check how much it changes
-                $percentage_rata = (int)$minimo->precio_oferta-(int)$this->product->precio_oferta/(float)$minimo->precio_oferta;
-                //compare between the oferta parameter with the reference...
-                $percentage_rata_relativo = (int)$minimo->precio_referencia-(int)$this->product->precio_oferta/(float)$minimo->precio_referencia;
-                if ($percentage_rata >= 0.25 && $percentage_rata_relativo >= 0.6) {
-                  \Notification::route('slack', env('SLACK_WEBHOOK_URL'))
+              if ((!$minimo->precio_oferta) || $minimo->precio_oferta > $this->product->precio_oferta) {
+                if ((boolean)$minimo->precio_oferta && $minimo->precio_oferta > $this->product->precio_oferta) {
+                  //check how much it changes
+                  $percentage_rata = (int)$minimo->precio_oferta-(int)$this->product->precio_oferta/(float)$minimo->precio_oferta;
+                  //compare between the oferta parameter with the reference...
+                  $percentage_rata_relativo = (int)$minimo->precio_referencia-(int)$this->product->precio_oferta/(float)$minimo->precio_referencia;
+                  if ($percentage_rata >= 0.4 && $percentage_rata_relativo >= 0.6) {
+                    \Notification::route('slack', env('SLACK_WEBHOOK_URL'))
                     ->notify(new \App\Notifications\AlertaRata($this->product, $minimo->precio_oferta, $this->product->precio_oferta));
+                  }
                 }
+                $minimo->precio_oferta = $this->product->precio_oferta;
               }
-              $minimo->precio_oferta = $this->product->precio_oferta;
-            }
-            if ((!$minimo->precio_tarjeta) || $minimo->precio_tarjeta > $this->product->precio_tarjeta) {
-              if ((boolean)$minimo->precio_tarjeta && $minimo->precio_tarjeta > $this->product->precio_tarjeta) {
-                //check how much it changes
-                $percentage_rata = (int)$minimo->precio_tarjeta-(int)$this->product->precio_tarjeta/(float)$minimo->precio_tarjeta;
-                //compare between the oferta parameter with the reference...
-                $percentage_rata_relativo = (int)$minimo->precio_referencia-(int)$this->product->precio_tarjeta/(float)$minimo->precio_referencia;
-                if ($percentage_rata >= 0.10 && $percentage_rata_relativo >= 0.7) {
-                  \Notification::route('slack', env('SLACK_WEBHOOK_URL'))
+              if ((!$minimo->precio_tarjeta) || $minimo->precio_tarjeta > $this->product->precio_tarjeta) {
+                if ((boolean)$minimo->precio_tarjeta && $minimo->precio_tarjeta > $this->product->precio_tarjeta) {
+                  //check how much it changes
+                  $percentage_rata = (int)$minimo->precio_tarjeta-(int)$this->product->precio_tarjeta/(float)$minimo->precio_tarjeta;
+                  //compare between the oferta parameter with the reference...
+                  $percentage_rata_relativo = (int)$minimo->precio_referencia-(int)$this->product->precio_tarjeta/(float)$minimo->precio_referencia;
+                  if ($percentage_rata >= 0.25 && $percentage_rata_relativo >= 0.7) {
+                    \Notification::route('slack', env('SLACK_WEBHOOK_URL'))
                     ->notify(new \App\Notifications\AlertaRata($this->product, $minimo->precio_tarjeta, $this->product->precio_tarjeta));
+                  }
                 }
+                $minimo->precio_tarjeta = $this->product->precio_tarjeta;
               }
-              $minimo->precio_tarjeta = $this->product->precio_tarjeta;
             }
+            // TODO: create historical, check minimum, etc etc
+            $minimo->save();
           }
-          // TODO: create historical, check minimum, etc etc
-          $minimo->save();
         }
         $this->product->save();
       }
