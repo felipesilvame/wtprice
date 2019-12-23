1495a4fd5798181fbdf452aa6bf613d4f8553ca0
Felipe Silva <felipe.silvame@alumnos.usm.cl>
2019-12-22 22:27:11 -0300

    update views

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,21 +6,35 @@
     <div class="row">
       @forelse($items as $item)
         @if($item->producto)
-        <div class="col-6 col-md-4 col-lg-3 mb-2">
+        <div class="col-12 col-md-6 col-lg-4 mb-2">
             <div class="card h-100">
-                <div class="card-body">
-                  <img src="{{ logo_tienda($item->producto->tienda->nombre )}}" alt="logo" style="max-width: 100%;max-height:50px">
-                    <p>Tienda: {{ $item->producto->tienda->nombre }}</p>
-                    <p>Nombre: {{ $item->producto->nombre }}</p>
-                    <p>Precio normal: {{ moneyFormat($item->precio_referencia, 'CLP')}}</p>
-                    @if($item->precio_oferta)
-                      <p>Precio Oferta:  <strong>{{ moneyFormat($item->precio_oferta, 'CLP')}}</strong></p>
+                <div class="card-body row">
+                    @if($item->producto->imagen_url)
+                    <div class="col-4 col-lg-12 text-center">
+                      <img src="{{ $item->producto->imagen_url }}" alt="img" style="max-width: 100%;max-height:150px">
+                    </div>
                     @endif
-                    @if($item->precio_tarjeta)
-                      <p>Precio Tarjeta:  <strong>{{ moneyFormat($item->precio_tarjeta, 'CLP')}}</strong></p>
-                    @endif
-                    <p>Ultima Actualizacion: {{ $item->updated_at->diffForHumans() }} ({{ $item->updated_at }})</p>
-                    <a href="{{ $item->producto->url_compra }}" target="_blank" class="btn btn-success">Ver</a>
+                    <div class="col-{{$item->producto->imagen_url ? '8' : '12'}}">
+                      <p class="m-0"><small>{{ $item->producto->tienda->nombre }}</small></p>
+                      <p>{{ $item->producto->nombre }}</p>
+                      @if($item->precio_tarjeta)
+                        <p class="m-0">Precio Tarjeta:  <strong>{{ moneyFormat($item->precio_tarjeta, 'CLP')}}</strong></p>
+                      @endif
+                      @if($item->precio_oferta)
+                        <p class="m-0">Precio Oferta:  <strong>{{ moneyFormat($item->precio_oferta, 'CLP')}}</strong></p>
+                      @endif
+                      <p class="m-0">
+                        @if($item->precio_oferta || $item->precio_tarjeta)
+                          <small><em><s>Precio normal: {{ moneyFormat($item->precio_referencia, 'CLP')}}</s></em></small>
+                        @else
+                          Precio normal: {{ moneyFormat($item->precio_referencia, 'CLP')}}
+                        @endif
+                      </p>
+                      <div class="actions d-flex">
+                        <a href="{{ $item->producto->url_compra }}" target="_blank" class="btn btn-success btn-sm">Ver</a>
+                        <abbr title="{{ $item->updated_at }}" class="ml-auto"><small><em>{{ $item->updated_at->diffForHumans() }}</em></small></abbr>
+                      </div>
+                    </div>
                 </div><!--card-body-->
             </div><!--card-->
         </div><!--col-->
