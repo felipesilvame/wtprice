aa4d4539a7a469d0b263ce3c76419736e87f1aea
Felipe Silva <felipe.silva@assetplan.cl>
2021-08-24 12:19:19 -0400

    added test falabella

--- app/Console/Commands/TestFalabellaCommand.php
+++ app/Console/Commands/TestFalabellaCommand.php
@@ -38,12 +38,41 @@ class TestFalabellaCommand extends Command
     public function handle()
     {
         $client = new \GuzzleHttp\Client();
-	$tienda = null;
-	$total_pages = 0;
-	$tienda = \App\Models\Tienda::whereNombre('Falabella')->first();
-	$_d = (string)'40';
-	$category = 'cat7090034';
-	$uri = 'https://www.falabella.com/s/browse/v1/listing/cl';
-	
+        $tienda = null;
+        $total_pages = 0;
+        $tienda = \App\Models\Tienda::whereNombre('Falabella')->first();
+        $_d = (string)'40';
+        $category = 'cat7090034';
+        $url = 'https://www.falabella.com/s/browse/v1/listing/cl';
+        $url .= "?categoryId=$category&page=$page_start&zone=13&channel=app&sortBy=product.attribute.newIconExpiryDate,desc&f.range.derived.variant.discount=$_d%25+dcto+y+más&f.derived.variant.sellerId=FALABELLA::SODIMAC::TOTTUS";
+        $response = null;
+        $data = null;
+        $total_pages = 0;
+        $options = [
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
+                'Accept'     => 'application/json',
+            ]
+        ];
+        if ((bool)env('APP_PROXY') && (bool)env('APP_PROXY_FALABELLA')) {
+            $options['proxy'] = env('APP_PROXY');
+            $options['verify'] = false;
+            $options['timeout'] = 15;
+        }
+        try {
+            //FALABELLA BLOCKS THE F*KING REQUESTS!!!!
+            usleep(1400000);
+            if (1) {
+                //deprecated, using classic curl
+                $response = $client->get($url, $options)->getBody()->getContents();
+            }
+        } catch (\Exception $e) {
+            Log::warning("SearchRataFalabella: No se ha obtenido respuesta satisfactoria de parte del request" . $tienda->nombre);
+            throw new \Exception("Error Processing Request", 1);
+        }
+        if ((boolean) $response) {
+            return 'Success';
+        } else return 'Failed';
+
     }
 }
